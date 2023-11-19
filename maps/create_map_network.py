f76ecc1cc44f3f5c9b15f76682db391982722cab
Vasu Padsumbia <vasu.padsumbia@gmail.com>
2023-11-19 19:53:49 +0100

    maps integration

--- maps/create_map_network.py
+++ maps/create_map_network.py
@@ -22,7 +22,13 @@ class create_map_network():
         self._destination = destination
 
     def create_street_network(self):
-        STREETGRAPH_FILENAME = self._palce.replace(',','')+'.graphml'
+        """
+        Generate graphml file for further transfer to GUI
+        Location name formate shall be like for example STREET_GRAPH_PLACE = Bremerhaven,Germany
+        and shall be saved as Bremerhaven_Germany.graphml
+        
+        """
+        STREETGRAPH_FILENAME = self._palce.replace(' ','_').replace(',','_')+'.graphml'
         STREETGRAPH_FILEPATH = ".//data//"+STREETGRAPH_FILENAME
         FORCE_CREATE = False
         #This Checks if the Streetnetwork File exists(or creation is overwritten using FORCE_CREATE)
@@ -34,28 +40,59 @@ class create_map_network():
         return STREETGRAPH_FILEPATH
 
     def create_area_graph(self):
+        """
+        Load graph from graphml file that we created in create_street_network() function
+        
+        """
         return ox.load_graphml(self.create_street_network())
     
     def plot_graph_map(self):
+        """
+        Generating graph map and plotting the graph of the desired location
         
+        """
         ec = ox.plot.get_edge_colors_by_sttr(self._graph, attr="length", num_bins=5)
 
         #otherwise, when num_bins is None (default), linearly map one color to each node/edge by value
-        ec = ox.plot.get_edge_colors_by_sttr(self._graph, attr="length")
+        #ec = ox.plot.get_edge_colors_by_sttr(self._graph, attr="length")
 
         #plot the graph with colored edges
         fig, ax = ox.plot_graph(self._graph, node_size=5, edge_color = ec, bgcolor="k")
 
+
     def find_shortest_path_between_two_points(self):
-        w = "walk"
+        """
+        
+        This function finds the best route for the desired destination. 
+        Please specify the type of network depending on the ride
+        
+        """
         node_point1 = ox.nearest_nodes(self._graph, self._origin[1], self._origin[0])
         node_point2 = ox.nearest_nodes(self._graph, self._destination[1], self._destination[0])
-        route_point1_point2 = ox.shortest_path(self._graph, node_point1, node_point2, weight=w)
+        route_point1_point2 = ox.shortest_path(self._graph, node_point1, node_point2, weight=self._network_type)
         return route_point1_point2
-    
-    def plot_graph_shortest_route(self):
-        return ox.plot_graph_route(self._graph, self.find_shortest_path_between_two_points(), orig_dest_size = 0, node_size=0)
+        
 
     def cartesian_coordinates(self):
-        return (self.find_shortest_path_between_two_points()['lat'], self.find_shortest_path_between_two_points()['lon'])
+        """
+
+        The data obtained from the find_shortest_path_between_two_points() function is in node ID form from OSMnx 
+        and so to convert those data to latitudes and longitudes we have to get the status from OSMnx analysis.
+        
+        """
+        coordinates=[]
+        g = self._graph
+        node_id = self.find_shortest_path_between_two_points()
+        for x in range(len(node_id)):
+            coordinates.append(g.nodes[node_id[x]]['y'])
+            coordinates.append(g.nodes[node_id[x]]['x'])
+        return coordinates
     
+    def plot_graph_shortest_route(self):
+        """
+
+        This function develops the graph of shortest route and imposes on the original map of the location
+        
+        """
+        return ox.plot_graph_route(self._graph, self.find_shortest_path_between_two_points(), orig_dest_size = 0, node_size=0)
+
--- maps/my_location.py
+++ maps/my_location.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 from maps.create_map_network import create_map_network
 
 centre_point = lat,lon = (53.540559, 8.5836)
-Destination = (53.55, 8.59)
+Destination = (53.53, 8.58)
 map = create_map_network('Bremerhaven,Germany', 'walk', centre_point, Destination)
 
 print(f'create street network(): {map.create_street_network()}')
 print(f'create area graph(): {map.create_area_graph()}')
 print(f'find shortest path between two points(): {map.find_shortest_path_between_two_points()}')
-print(f'plot graph shortest route(): {map.plot_graph_shortest_route()}')
 print(f'cartesian coordinates(): {map.cartesian_coordinates()}')
+print(f'plot graph shortest route(): {map.plot_graph_shortest_route()}')
