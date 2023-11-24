ebe14767feea4756792dd567faa098c60e89e31f
Vasu Padsumbia <vasu.padsumbia@gmail.com>
2023-11-24 14:29:56 +0100

    First commit
    Author:   VasuPadsumbia <vasu.padsumbia@gmail.com

--- maps/my_location.py
+++ maps/my_location.py
@@ -2,18 +2,34 @@ import osmnx as ox
 import networkx as nx
 import matplotlib.pyplot as plt
 from maps.create_map_network import create_map_network
-from sensor.dgps.DGPS import connect_pksi_dgps
+from sensor.dgps.DGPS import *
+import json
 
 
-centre_point = (53.540559, 8.5836)
-Destination = (53.53, 8.58)
 
-gps = connect_pksi_dgps()
-#print(f'Connecting with GPS: {gps.connect_piksi()}')
-print(f'Getting ECEF Longitudenal and Latitude data: {gps.get_data()}')
+config_path = f'{os.path.abspath(os.path.join(os.path.dirname(__file__),""))}/map_coordinates.json'
 
+try:
+    with open(config_path, "r") as config_file:
+        data = json.load(config_file)
+        lat = data['lat']
+        lon = data['lon']
+        config_file.close()
+        
+except JSONDecodeError as e:
+    print("Failed to read JSON, return code %d\n", e)
 
+centre_point = (lat, lon)
+Destination = (53.517991, 8.60)
+
+"""try:
+    gps = connect_pksi_dgps()
+    print(f'Getting Longitudenal and Latitude data: {gps.get_data()}')
+
+except KeyboardInterrupt:
+    pass
 """
+
 map = create_map_network('Bremerhaven,Germany', 'walk', centre_point, Destination)
 
 print(f'create street network(): {map.create_street_network()}')
@@ -21,4 +37,3 @@ print(f'create area graph(): {map.create_area_graph()}')
 print(f'find shortest path between two points(): {map.find_shortest_path_between_two_points()}')
 print(f'cartesian coordinates(): {map.cartesian_coordinates()}')
 print(f'plot graph shortest route(): {map.plot_graph_shortest_route()}')
-"""
--- sensor/dgps/DGPS.py
+++ sensor/dgps/DGPS.py
@@ -1,8 +1,8 @@
-import os
-import argparse
+import os, sys, argparse
+sys.path.append("../../")
 from sbp.client.drivers.network_drivers import TCPDriver
 from sbp.client import Handler, Framer
-from sbp.navigation import SBP_MSG_POS_ECEF
+from sbp.navigation import *
 from json.decoder import JSONDecodeError
 import json
 from helper import *
@@ -24,7 +24,20 @@ class connect_pksi_dgps():
         Args:
 
         """
-
+        self.flag = 0.0
+        self.n = 0.0
+        self.e = 0.0
+        self.d = 0.0
+        self.lat = 0.0
+        self.lon = 0.0
+        self.height = 0.0
+        self.h = 0.0
+        self.v_n = 0.0
+        self.v_e = 0.0
+        self.v_d = 0.0
+        self.wn = 0
+        self.tow = 0
+        
         self.config_path = f'{os.path.abspath(os.path.join(os.path.dirname(__file__),"../.."))}/Configure.json'
         #self.config_path = "Configure.json"
 
@@ -38,40 +51,83 @@ class connect_pksi_dgps():
         except JSONDecodeError as e:
             print("Failed to read JSON, return code %d\n", e)
 
-    def connect_piksi(self):
-        ''' Creates Piksi connection'''
-        def on_connect(client, userdata, flag, rc):
-            if rc == 0:
-                print('Connected to MQTT Broaker: {}'.format(self.broker))
-
-            else:
-                print("Failed to connect, return code %d\n", rc)
-
-        driver = TCPDriver(self.IP_add, self.port)
-        source = Handler(Framer(driver.read, None, verbose=True))
-        print(f'Connection Eshtablished for piksi at IP {self.IP_add}')
-        return source
 
     def get_data(self):
-        ''' Publish data to the broaker'''
-        try:
-            for msg, metadata in self.connect_piksi.filter(SBP_MSG_POS_ECEF):
-                    # Print out the N, E, D coordinates of the baseline
-                    print("he position solution message reports absolute Earth Centered Earth Fixed (ECEF)" 
-                          "coordinates and the status (single point vs pseudo-absolute RTK) of"
-                           "the position solution. If the rover receiver knows the surveyed position of"
-                           "the base station and has an RTK solution, this reports a pseudo-absolute"
-                           "position solution using the base station position and the rover's RTK"
-                           "baseline vector.")
-                    print(f'Data Recieving from piksi at IP {msg.sender}')
-                    print(f'accuracy of data {msg.accuracy}')
-                    print("Latitude: %.4f, Longitude: %.4f" % (msg.x , msg.y ))
-                    #centre_point = lat,lon = (msg.x * 1e-3, msg.y * 1e-3)
-                    #graph = ox.graph_from_point(centre_point, dist=1000, dist_type='bbox', 
-                    #        network_type='walk')
-                    #fig, ax = ox.plot_graph(graph, show=False, close=False, 
-                    #                        bgcolor='w',node_color='b', node_size=2)
-                    #plt.plot(lon, lat, marker="o", markersize=5, markeredgecolor="red", markerfacecolor="red", alpha=0.5)
-                    #plt.show()         
-        except:
-            print("Error getting data!")
+        iteration = 0
+        ''' Creates Piksi connection'''
+        with TCPDriver(self.IP_add, self.port) as driver:
+            with Handler(Framer(driver.read, None, verbose=True)) as source:
+                print(f'Connection Eshtablished for piksi at IP {self.IP_add}')
+        
+                ''' Getting data'''
+                try:
+                    #msg_list = [SBP_MSG_BASELINE_NED, SBP_MSG_POS_LLH,
+                    #                SBP_MSG_VEL_NED, SBP_MSG_GPS_TIME]
+                    
+                    for msg, metadata in source.filter(SBP_MSG_POS_LLH):
+                            iteration = 1 + iteration
+                            print("he position solution message reports absolute Earth Centered Earth Fixed (ECEF)" 
+                                  "coordinates and the status (single point vs pseudo-absolute RTK) of"
+                                   "the position solution. If the rover receiver knows the surveyed position of"
+                                   "the base station and has an RTK solution, this reports a pseudo-absolute"
+                                   "position solution using the base station position and the rover's RTK"
+                                   "baseline vector.")
+                            print(f'Data Recieving from piksi at IP {msg.sender}')
+                            print(f'accuracy of data {msg.h_accuracy}')
+                            print("Latitude: %.4f, Longitude: %.4f" % (msg.lat , msg.lon ))
+
+                            # LLH position in deg-deg-m
+                            if msg.msg_type == 522:
+                                self.lat = msg.lat
+                                self.lon = msg.lon
+                                self.h = msg.height
+
+                            # RTK position in mm (from base to rover)
+                            elif msg.msg_type == 524:
+                                self.n = msg.n
+                                self.e = msg.e
+                                self.d = msg.d
+                                self.flag = msg.flags
+
+                            # RTK velocity in mm/s
+                            elif msg.msg_type == 526:
+                                self.v_n = msg.n
+                                self.v_e = msg.e
+                                self.v_d = msg.d
+
+                            # GPS time
+                            elif msg.msg_type == 258:
+                                self.wn = msg.wn
+                                self.tow = msg.tow  # in millis
+
+                            else:
+                                pass
+                            
+                            self.log()
+                            #print(self.whole_string())
+                            # f.write(line)
+                            # f.write('\n')
+
+                except iteration==10:
+                    print("Error getting data!")
+    
+    
+    def whole_string(self):
+        '''
+        Returns all the data as a string
+        '''
+
+        return('%.0f\t%.0f\t%2.8f\t%2.8f\t%4.6f\t%6.0f\t%6.0f\t%6.0f\t'
+               '%6.0f\t%6.0f\t%6.0f\t%.0f\t' %
+               (self.wn, self.tow, self.lat, self.lon, self.h, self.n, self.e,
+                self.d, self.v_n, self.v_e, self.v_d, self.flag))
+    
+    def log(self):
+        data_JSON =  {
+	        "tow": self.tow,
+	        "lat": self.lat,
+	        "lon": self.lon,
+	        "height": self.height,
+        }
+        with open("map_coordinates.json", "w") as write_file:
+            json.dump(data_JSON, write_file)
