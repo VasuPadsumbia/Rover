91e5037403c8a2d6ff46e7f09228d2b84fe0bb60
VasuPadsumbia <vasu.padsumbia@gmail.com>
2023-12-05 22:02:58 +0100

    simulaiton setup

--- sensor/dgps/simulator_mode.bash
+++ sensor/dgps/simulator_mode.bash
@@ -2,6 +2,6 @@
 
 echo " "
 echo "Setting GPS to simulator mode ..."
-python3 setup_piksi.py -f settings_file/settings.ini
+python3 /home/outdoor/piksi_tools/piksi_tools/settings.py -t -p 195.37.48.193:55555 write_from_file settings_file/settings.ini
 echo " "
     echo "Task finished."
